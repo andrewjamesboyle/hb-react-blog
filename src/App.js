8fc00a71907694b53052bb080ea6d268b2b56e8c
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-14 15:27:02 -0700

    attempt to fix deploy preview

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+
 import './App.css';
 
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import Home from './components/Home/Home';
+import { resolveConfigFile } from 'prettier';
 
 function App() {
   return (
