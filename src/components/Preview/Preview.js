e595bb938d8e7499a7836618fc72c77e0823c870
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-14 15:06:48 -0700

    text alignment functionality

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subtitle, font, text }) {
+export default function Preview({ title, subtitle, font, align, text }) {
   return (
     <>
-      <div className={`preview ${font}`} >
+      <div className={`preview ${font}`} style={{ textAlign: `${align}` }} >
         <h1>{title}</h1>
         <h3>{subtitle}</h3>
         <p>{text}</p>
