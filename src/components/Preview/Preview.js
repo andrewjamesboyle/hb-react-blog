20192ca1356b1cb2f9d00697513463aad8920b01
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-14 13:54:58 -0700

    dynamic subtitle previwing

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title }) {
+export default function Preview({ title, subtitle }) {
   return (
     <>
       <div>
         <h1>{title}</h1>
+        <h3>{subtitle}</h3>
       </div>
     </>
   );
